9e00793cd9fc4c8fbaf4b5e09a9d64716952a951
Morgan Hondros <morganhondros@gmail.com>
2019-02-22 19:26:52 -0500

    After Lecture 202.

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Firebase
 
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // Declare instance variables here
 
@@ -26,9 +26,12 @@ class ChatViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         //TODO: Set yourself as the delegate and datasource here:
         
-        
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
+        configureTableView()
         
         //TODO: Set yourself as the delegate of the text field here:
 
@@ -39,7 +42,7 @@ class ChatViewController: UIViewController {
         
 
         //TODO: Register your MessageCell.xib file here:
-
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
         
     }
 
@@ -50,10 +53,21 @@ class ChatViewController: UIViewController {
     
     
     //TODO: Declare cellForRowAtIndexPath here:
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+        
+        let messageArray = ["First Message", "Second Message", "Third Message"]
+        
+        cell.messageBody.text = messageArray[indexPath.row]
+        
+        return cell
+    }
     
     
     //TODO: Declare numberOfRowsInSection here:
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
     
     
     
@@ -62,7 +76,11 @@ class ChatViewController: UIViewController {
     
     
     //TODO: Declare configureTableView here:
-    
+    func configureTableView() {
+        messageTableView.rowHeight = UITableView.automaticDimension
+        messageTableView.estimatedRowHeight = 120.0
+        
+    }
     
     
     ///////////////////////////////////////////
